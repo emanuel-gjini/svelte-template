3548aae5410c9b5662a63f3ee9d864491dda77e6
Emanuel Gjini <emanuel_gjini@yahoo.com>
2021-07-08 14:01:23 +0200

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Router, Route, Link } from "svelte-routing";
+	import Home from "./pages/Home.svelte";
+	import About from "./pages/About.svelte";
+
+	export let url = "";
+</script>
+
+<Router {url}>
+	<nav>
+		<Link to="/">Home</Link>
+		<Link to="about">About</Link>
+	</nav>
+
+	<div>
+		<Route path="/"><Home /></Route>
+		<Route path="about" component={About} />
+	</div>
+</Router>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,14 @@
+<script>
+    const currentDate = new Date();
+</script>
+<main>
+    <h1>Welcome to Home Page hello</h1>
+    {currentDate}
+</main>
+<style>
+    h1 {
+        text-transform: uppercase;
+        font-size: 3em;
+        font-weight: 100;
+    }
+</style>
